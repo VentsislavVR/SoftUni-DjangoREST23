7247a90792fc6768c6bca51c1623eb46c0909ae0
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2024-01-17 17:39:48 +0200

    drf demo

--- 07.DRF/drf_demos/drf_demos/api/urls.py
+++ 07.DRF/drf_demos/drf_demos/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from drf_demos.api.views import AuthorsApiView
+from drf_demos.api.views import AuthorsApiView, AuthorsView
 
 urlpatterns = (
     path('authors/',AuthorsApiView.as_view(),name='api_list_authors'),
+    path('authors-ssr/',AuthorsView.as_view(),name='list_authors'),
 )
--- 07.DRF/drf_demos/drf_demos/api/views.py
+++ 07.DRF/drf_demos/drf_demos/api/views.py
@@ -1,9 +1,19 @@
 # from django.shortcuts import render
 # from rest_framework.decorators import api_view
-
-from rest_framework import generics as api_views, serializers
+from time import sleep
+from django.views import generic as views
+from rest_framework import generics as api_views, serializers, permissions
 
 from drf_demos.api.models import Author
+
+
+class AuthorsView(views.ListView):
+    queryset = Author.objects.all()
+    template_name = 'authors.html'
+
+    def get(self, request, *args, **kwargs):
+        sleep(5)
+        return super().get(request, *args, **kwargs)
 
 
 # Create your views here.
@@ -17,6 +27,27 @@ class AuthorSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class AuthorsApiView(api_views.ListAPIView):
+# author_serializer = AuthorSerializer(
+#     instance=Author.objects.all(),
+# )
+# print(author_serializer.data)
+
+# author_serializer_json = AuthorSerializer(
+#     data={"id": 1,
+#           "first_name": "Marli",
+#           "last_name": "Racheva",
+#           "followers_count": 0}
+# )
+# print(author_serializer_json.is_valid())
+class AuthorsApiView(api_views.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+    )
+
+    def get(self, request, *args, **kwargs):
+        print(request.session.get('count', None))
+        request.session['count'] = 5
+        print(request.user)
+        return super().get(request, *args, **kwargs)
